93be0c0c0ef3a8a02474bfd02ecf1589ecf1fa5d
Thoriqfm <tfarrelm@gmail.com>
2026-05-31 10:24:44 +0700

    card component

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Card from "./Card.jsx";
+
+function App() {
+  return (
+    <>
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </>
+  );
+}
+
+export default App;
