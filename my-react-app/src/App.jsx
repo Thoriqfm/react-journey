8d839393ad08bf3aaf3d2fbe5d83ff4af0674f0c
Thoriqfm <tfarrelm@gmail.com>
2026-06-01 09:47:42 +0700

    props

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,9 +1,15 @@
-import Button from "./Button.jsx";
+import Student from "./Student";
 
 function App() {
   return (
-    <Button />  
-  );
+    <>
+      <Student name="John Doe" age={30} isStudent={true} />
+      <Student name="Patrick Jane" age={25} isStudent={false} />
+      <Student name="Spongebob Squarepants" age={50} isStudent={true} />
+      <Student name="Sandy" age={27} isStudent={true} />
+      <Student/>
+    </>
+  )
 }
 
 export default App;
