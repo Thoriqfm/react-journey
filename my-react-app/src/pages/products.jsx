0b5fdd572e596ba9e288c72febdadf861afd2ce2
Thoriqfm <tfarrelm@gmail.com>
2026-06-05 19:35:31 +0700

    rendering list

--- my-react-app/src/pages/products.jsx
+++ my-react-app/src/pages/products.jsx
@@ -1,24 +1,44 @@
 import CardProduct from "../components/fragments/CardProduct";
 
+const products = [
+  {
+    id: 1,
+    name: "Sepatu baru",
+    description:
+      "Sepatu baru dengan desain yang modern dan nyaman untuk digunakan sehari-hari.",
+    price: "$599.99",
+    image: "/images/shoes-1.jpg",
+  },
+  {
+    id: 2,
+    name: "Sepatu lama",
+    description:
+      "Sepatu lama dengan desain yang klasik dan nyaman untuk digunakan sehari-hari.",
+    price: "$499.99",
+    image: "/images/shoes-1.jpg",
+  },
+  {
+    id: 3,
+    name: "Sepatu olahraga",
+    description:
+      "Sepatu olahraga dengan desain yang modern dan nyaman untuk digunakan sehari-hari.",
+    price: "$699.99",
+    image: "/images/shoes-1.jpg",
+  },
+];
+
 const ProductsPage = () => {
   return (
     <div className="flex justify-center py-5">
-      <CardProduct>
-        <CardProduct.Header image="/images/shoes-1.jpg" />
-        <CardProduct.Body title="Sepatu baru">
-          Sepatu baru dengan desain yang modern dan nyaman untuk digunakan
-          sehari-hari.
-        </CardProduct.Body>
-        <CardProduct.Footer price="$599.99" />
-      </CardProduct>
-      <CardProduct>
-        <CardProduct.Header image="/images/shoes-1.jpg" />
-        <CardProduct.Body title="Sepatu baru">
-          Sepatu baru dengan desain yang modern dan nyaman untuk digunakan
-          sehari-hari.
-        </CardProduct.Body>
-        <CardProduct.Footer price="$599.99" />
-      </CardProduct>
+      {products.map((product) => (
+        <CardProduct key={product.id}>
+          <CardProduct.Header image={product.image} />
+          <CardProduct.Body title={product.name}>
+            {product.description}
+          </CardProduct.Body>
+          <CardProduct.Footer price={product.price} />
+        </CardProduct>
+      ))}
     </div>
   );
 };
